e554c614c3509675ce0c88eac0d1044992c7edb9
riyazedd <riyaz.shrestha00@gmail.com>
2022-11-11 19:37:02 +0545

    update

--- Practice/practice.py
+++ Practice/practice.py
@@ -1,7 +1,9 @@
+print("------CALCULATOR------")
+
 num1=int(input("Enter first number= "))
 num2=int(input("Enter second number= "))
 
-operator=input("Enter your opeation(*,+,-,/)= ")
+operator=input("Enter your operation(*,+,-,/)= ")
 
 if operator=='*':
     operation=num1*num2
@@ -15,4 +17,4 @@ else:
     print("---OPERATION INVALID---")
     exit()
 
-print(f"Result={operation}")
+print(f"---Result:---\n{num1}{operator}{num2}={operation}")
