5241c985a0d25651e8a09185e713dcb13d929066
riyazedd <riyaz.shrestha00@gmail.com>
2022-12-02 18:36:17 +0545

    practice

--- Day 16/calculator.py
+++ Day 16/calculator.py
@@ -1,22 +1,21 @@
-from operations import add,sub,mul,div
+from operations import add,sub,mul,div #operations bata import gareko
 
 print("Operations:\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Dvision")
+opt=int(input("Enter Your Operation: "))
 
-opt=int(input("Enter your choice: "))
-a=int(input("Enter first number: "))
-b=int(input("Enter second number: "))
+if opt<=4: #if operations choice 4 samma vayo vane matra execute hunxa
+    a=int(input("Enter first number: "))
+    b=int(input("Enter second number: "))
+    def calculator():
+        if opt==1:
+            return add(a,b)
+        elif opt==2:
+            return sub(a,b)
+        elif opt==3:
+            return mul(a,b)
+        else:
+            return div(a,b)
+    print(f"Result= {calculator()}")
 
-def calculator():
-    if opt==1:
-        return add(a,b)
-    elif opt==2:
-        return sub(a,b)
-    elif opt==3:
-        return mul(a,b)
-    elif opt==4:
-        return div(a,b)
-    else:
-        return ("Choose valid option.")
-
-print(f"Result= {calculator()}")
-
+else:
+    print("Enter Valid Operation")
